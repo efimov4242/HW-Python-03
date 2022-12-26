d10f4066b4e9632fd6096cc2f12b9a8aee9f0367
Ilya <efimov4242@mail.ru>
2022-12-26 22:23:27 +0400

    ДЗ к 3 семинару

--- 02.py
+++ 02.py
@@ -3,12 +3,10 @@
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
 
-list = [2, 3, 4, 5, 6]
+list = [2, 3, 5, 6]
+new_list = []
 
-def get_mult_couples_elements(list):
-	mult = 0
-	for i in range(len(int(list)/2), 1):
-		mult[i] = list[i] * list[len(list) - i - 1]
-	return mult
+for i in range(len(list) - 2):
+    new_list.append(list[i] * list[len(list) - i - 1])
 
-print(get_mult_couples_elements(list))
+print(new_list)
